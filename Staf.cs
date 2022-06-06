4f582b6c96d901933c775844539ed47e337fc0c9
Mitatsej <88859348+Mitatsej@users.noreply.github.com>
2022-06-06 11:49:07 +0200

    Add files via upload

---
+++ Staf.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETickets.BLL
+{
+    public class Staf
+    {
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public DateTime Bithdate { get; set; }
+        public bool Gender { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Settlement { get; set; }
+        public byte[] Image { get; set; }
+        public int PositionID { get; set; }
+        public int QualificationID { get; set; }
+        public int TrainingID { get; set; }
+        public int MunicipalityID { get; set; }
+        public int NationalityID { get; set; }
+        public int StateID { get; set; }
+        public int CitizenshipID { get; set; }
+
+
+
+
+
+
+    }
+}
