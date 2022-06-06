be9fe3e0c1eaa0d325036dd76aa2ad0cce808bd6
Mitatsej <88859348+Mitatsej@users.noreply.github.com>
2022-06-06 13:30:00 +0200

    Add files via upload

---
+++ ETickets.BO/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETickets.BO
+{
+    public class Class1
+    {
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Surname { get; set; }
+
+        public int PersonalNuMBER { get; set; }
+
+        public int Ticketnumber { get; set; }
+
+
+        public int InsertBy { get; set; }
+        public DateTime InsertDate { get; set; }
+        public int LUN { get; set; }
+        public int LUB { get; set; }
+        public DateTime LUD { get; set; }
+
+
+
+
+
+    }
+}
